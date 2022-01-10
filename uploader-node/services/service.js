0c349051ed1d5f7a6ac51ddf40c221c992753ddd
Moon Juhan <codemike@naver.com>
2022-01-10 18:36:10 +0900

    feat: add initialize webdriver with metamask extension installed

--- uploader-node/services/service.js
+++ uploader-node/services/service.js
@@ -1,23 +1,27 @@
-const localWebDriver = 'http://localhost:9515'
 const { sleep, openURL, clickElementByText } = require('./helpers')
 const webdriver = require('selenium-webdriver')
-const driver = new webdriver.Builder().usingServer(localWebDriver).forBrowser('chrome').build()
 const By = webdriver.By
+let driver
 
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+const setupWebdriver = async () => {
+  await sleep(100)
 
-const openURL = async (url) => {
-  await driver.get(url)
+  const chrome = require('selenium-webdriver/chrome')
+  const options = new chrome.Options()
+  options.addExtensions(`./${process.env.METAMASK_EXTENSION_NAME}`)
+
+  driver = new webdriver.Builder()
+    .usingServer(process.env.LOCAL_WEBDRIVER_URL)
+    .forBrowser('chrome')
+    .setChromeOptions(options)
+    .build()
 }
+
+setupWebdriver()
+
 
 exports.getStatus = async () => {
   try {
-    await openURL('https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=ko')
-
-    await sleep(5000)
-
-    const button = await driver.findElement(By.className('dd-Va g-c-wb g-eg-ua-Uc-c-za g-c-Oc-td-jb-oa g-c'))
-    button.click()
   } catch (err) {
     console.log(err)
     throw Error(err)
