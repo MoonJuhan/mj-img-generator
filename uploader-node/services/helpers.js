8b0c7856512920f06076d9998cb32cdf1095d6d7
Moon Juhan <codemike@naver.com>
2022-01-11 17:54:31 +0900

    feat: add function to control metamask  window

--- uploader-node/services/helpers.js
+++ uploader-node/services/helpers.js
@@ -1,3 +1,6 @@
+const webdriver = require('selenium-webdriver')
+const By = webdriver.By
+
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 const openURL = async (driver, url, sleepTime) => {
@@ -14,6 +17,31 @@ const clickElementByText = async (driver, type, text, sleepTime) => {
       await elements[i].click()
       await sleep(sleepTime || 0)
       return
+const controllMetamaskWindow = async (driver, callback, stack = 0) => {
+  stack++
+  const data = await driver.getAllWindowHandles()
+  const windowList = []
+
+  for (let i = 0; i < data.length; i++) {
+    await driver.switchTo().window(data[i])
+    const title = await driver.getTitle()
+    windowList.push({
+      title,
+      id: data[i],
+    })
+  }
+
+  if (windowList.find((el) => el.title === 'MetaMask Notification')) {
+    await driver.switchTo().window(windowList.find((el) => el.title === 'MetaMask Notification').id)
+    await callback()
+    await driver.switchTo().window(windowList.find((el) => el.title !== 'MetaMask Notification').id)
+  } else {
+    if (stack < process.env.DELAY_TIME) {
+      console.log(`controllMetamaskWindow Func retry to find MetaMask Window in ${stack} times.`)
+      await sleep(250)
+      await controllMetamaskWindow(driver, callback, stack)
+    } else {
+      console.log(`controllMetamaskWindow Func fail to find MetaMask Window`)
     }
   }
 }
@@ -22,4 +50,5 @@ module.exports = {
   sleep,
   openURL,
   clickElementByText,
+  controllMetamaskWindow,
 }
